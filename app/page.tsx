ea96aee7343589357af854aabeaf1f0f179bbb21
ArtJustine <152577317+ArtJustine@users.noreply.github.com>
2025-03-19 01:10:10 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image"
 import Link from "next/link"
-import { ChevronRight, CuboidIcon as Cube } from "lucide-react"
+import { ChevronRight } from "lucide-react";
 
 export default function Home() {
   return (
